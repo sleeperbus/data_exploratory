e981b618026d7ab6e253a93b0cbb39ed19a6cb65
Hojoon Ji <sleeperbus@gmail.com>
2015-12-21 01:16:05 +0000

    ..

--- Hierarchical cluster analysis/vote_republican.R
+++ Hierarchical cluster analysis/vote_republican.R
@@ -10,3 +10,36 @@ axis(1, at=seq_along(years), labels = years)
 axis(2)
 title("Votes for Republican Candidate\n in Presidential Elections\n 
       (each line is a country - over the years")
+# 이런 방식으로는 패턴을 찾기 힘드라. 
+# heatmap 을 그려보자.
+library(dendextend)
+arcsin_transformation <- function(x) asin(x/100)
+
+dend_NA <- votes.repub %>% is.na %>% 
+  dist %>% hclust %>% as.dendrogram %>% ladderize
+
+dend <- votes.repub %>% arcsin_transformation %>% 
+  dist %>% hclust(method="com") %>% as.dendrogram %>%
+  rotate(labels(dend_NA)) %>%
+  color_branches(k=3)
+
+some_col_func <- colorspace::diverge_hcl
+
+# par(mar=c(3,3,3,3))
+# library(gplots)
+gplots::heatmap.2(as.matrix(votes.repub),
+                  main="Votes for \n Republican Presidential Candidate\n (clustered using complete)",
+                  srtCol = 60,
+                  dendrogram = "row",
+                  Rowv = dend, 
+                  Colv = "NA",
+                  trace = "none",
+                  margins = c(3, 6),
+                  key.xlab = "% Votes for Republican\n Presidential Candidate",
+                  labCol = years,
+                  denscol = "grey",
+                  density.info = "density",
+                  col = some_col_func
+                  )
+
+
